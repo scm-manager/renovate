bef632ee82bc301f822ea11c56b670f3bcb0b7ce
tzerr <thomas.zerr@cloudogu.com>
2023-08-01 20:49:01 +0200

    work in progress

--- lib/modules/platform/scmm/mapper.ts
+++ lib/modules/platform/scmm/mapper.ts
@@ -2,5 +2,17 @@ import type { Pr as RenovatePr } from '../types';
 import type { PullRequest as SCMPullRequest } from './types';
 
 export function mapPrFromScmToRenovate(pr: SCMPullRequest): RenovatePr {
-  return {};
+  return {
+    sourceBranch: pr.source,
+    createdAt: pr.creationDate,
+    closedAt: pr.closeDate,
+    hasAssignees: pr.reviewer.length > 0,
+    labels: pr.labels,
+    number: parseInt(pr.id),
+    reviewers: pr.reviewer.map((review) => review.displayName),
+    //TODO is this correct
+    state: pr.status,
+    title: pr.title,
+    isDraft: pr.status === 'DRAFT',
+  };
 }
